abb3d3cfebea5eccf1e8866c115761bfb98670f8
Raghvendra Sahu <raghvendra.19934@gmaial.com>
2019-04-12 15:18:54 +0530

    by rrrr

--- app/src/main/java/com/ics/newapp/fregment/BuyerFragment.java
+++ app/src/main/java/com/ics/newapp/fregment/BuyerFragment.java
@@ -1,12 +1,19 @@
 package com.ics.newapp.fregment;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.ics.newapp.R;
 
@@ -21,9 +28,46 @@ public class BuyerFragment extends Fragment {
 
     }
 
+//    @Override
+//    public void onPrepareOptionsMenu(Menu menu) {
+//        MenuItem item=menu.findItem(R.id.card_shopping);
+//        item.setVisible(true);
+//
+////            item2.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+////                @Override
+////                public boolean onMenuItemClick(MenuItem item) {
+////                    Fragment view_creat=new Create_Activity();
+////                    FragmentManager fragmentManager = getFragmentManager();
+////                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+////                    fragmentTransaction.replace(R.id.content_frame,view_creat);
+////                    fragmentTransaction.addToBackStack(null);
+////                    fragmentTransaction.commit();
+////
+////
+////                    return false;
+////                }
+////            });
+//
+//
+//    }
+
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+       // setHasOptionsMenu(true);
+
+        LinearLayout layout = (LinearLayout)view.findViewById(R.id.linear);
+        for (int i = 0; i < 5; i++) {
+            ImageView imageView = new ImageView(view.getContext());
+            imageView.setId(i);
+            imageView.setPadding(2, 2, 2, 2);
+            imageView.setImageBitmap(BitmapFactory.decodeResource(
+                    getResources(), R.drawable.dd2));
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            imageView.setMinimumWidth(12);
+            layout.addView(imageView);
+        }
     }
 }
