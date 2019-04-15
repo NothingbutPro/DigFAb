ce5f34e6450da4c0c7a515d245ed9e83eee54a2d
Raghvendra Sahu <raghvendra.19934@gmaial.com>
2019-04-15 12:54:56 +0530

    by rrrr

--- app/src/main/java/com/ics/newapp/Navigation.java
+++ app/src/main/java/com/ics/newapp/Navigation.java
@@ -140,7 +140,10 @@ public class Navigation extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.main_menu_group) {
+
+//            Intent intent = new Intent(Navigation.this,ProfileActivity.class);
+//            startActivity(intent);
             return true;
         }
 
